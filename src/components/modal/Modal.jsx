9c65699e7280472024cf6c200ea021bfb76bd475
sergey <makarkapar@gmail.com>
2024-04-06 23:52:14 +0300

    commit

--- src/components/modal/Modal.jsx
+++ src/components/modal/Modal.jsx
@@ -1,11 +1,13 @@
-import React, { useEffect } from "react"
+import React from "react"
 import { Link } from "react-router-dom";
 import logo from "../../svg/logo.svg"
-import phoneIcon from "../../svg/phone.svg";
-import locationIcon from "../../svg/location.svg";
+import PhoneIcon from "../../svg/phone-w.svg";
+import locationIcon from "../../svg/location-w.svg";
+import closeIcon from "../../svg/close.svg";
 import data from "../../data/catalogData.json";
 import { observer } from "mobx-react";
 import modalStore from "../../store/modalStore";
+import "./modal.scss";
 
 const Modal = observer(() => {
   const [isData, setIsData] = React.useState(null);
@@ -13,17 +15,18 @@ const Modal = observer(() => {
     setIsData(data[modalStore.id]);
   }, [modalStore.id])
   return (
-    <div className="modal__wrapper">
+    <div className={modalStore.stateModal ? "modal__wrapper show" : "modal__wrapper"}>
       <div className="modal">
-        <div className="col">
+        <button className="modal__close" onClick={() => modalStore.setStateModal()}>
+          <img src={closeIcon} className="modal__close--img" />
+        </button>
+        <div className="modal__img-wrapper">
           <img className="modal__img" src={isData?.img} alt={isData?.name} />
         </div>
         <div className="col">
           <div className="row">
-            <h3 className="modal__title">{isData?.name}</h3>
-            <p className="modal__description">{isData?.description}</p>
             <Link to="tel:+78652550224" className="modal__contact">
-              <img className="modal__contact--icon" src={phoneIcon} />
+              <img className="modal__contact--icon" src={PhoneIcon} />
               +78652550224
             </Link>
             <Link to="https://yandex.ru/maps/-/CDRmaO0Z" target="_blank" className="modal__contact">
